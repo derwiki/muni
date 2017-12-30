7c2ab4e7e9b7fab69234095a83b1783bd76941d9
Adam Derewecki <derewecki@gmail.com>
2017-12-29 18:22:45 -0800

    retry up to 5 times

--- server.rb
+++ server.rb
@@ -9,19 +9,31 @@ RS = {
 	r91: Muni::Route.find(91)
 }
 
-get '/' do
-  stops = [
-    RS[:r10].inbound.stop_at('Wisconsin St & Madera St'),
-    RS[:r19].inbound.stop_at('De Haro St & 23rd St'),
-    RS[:r48].inbound.stop_at('25th St & Wisconsin St'),
-    RS[:r48].outbound.stop_at('25th St & Wisconsin St'),
-  ]
-  routes = {}
-  stops.map do |stop|
-    direction = stop.direction !~ /___O_/ ? 'Inbound' : 'Outbound'
-    key = "#{stop.route_tag} #{direction} at #{stop.title}"
-    routes[key] = stop.predictions
-  end
+STOPS = [
+	RS[:r10].inbound.stop_at('Wisconsin St & Madera St'),
+	RS[:r19].inbound.stop_at('De Haro St & 23rd St'),
+	RS[:r48].inbound.stop_at('25th St & Wisconsin St'),
+	RS[:r48].outbound.stop_at('25th St & Wisconsin St'),
+]
 
-  haml :index, locals: { routes: routes }
+get '/' do
+  begin
+		retries ||= 0
+		routes = {}
+		STOPS.map do |stop|
+			direction = stop.direction !~ /___O_/ ? 'Inbound' : 'Outbound'
+			key = "#{stop.route_tag} #{direction} at #{stop.title}"
+			routes[key] = stop.predictions
+		end
+
+		haml :index, locals: { routes: routes }
+
+  rescue StandardError => e
+    puts "#{e.class}: #{e.message}"
+    puts "retries #{retries}"
+    if (retries += 1) < 5
+      puts "retrying"
+      retry
+    end
+  end
 end
