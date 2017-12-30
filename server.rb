443d8c448e7a9cf0a7952e7e9cd077cf1de855dc
Adam Derewecki <derewecki@gmail.com>
2017-12-29 20:38:26 -0800

    Bus image

--- server.rb
+++ server.rb
@@ -3,17 +3,17 @@ require 'muni'
 require 'haml'
 
 RS = {
-	r48: Muni::Route.find(48),
-	r19: Muni::Route.find(19),
-	r10: Muni::Route.find(10),
-	r91: Muni::Route.find(91)
+  r48: Muni::Route.find(48),
+# r19: Muni::Route.find(19),
+  r10: Muni::Route.find(10),
+# r91: Muni::Route.find(91)
 }
 
 STOPS = [
-	RS[:r10].inbound.stop_at('Wisconsin St & Madera St'),
-	RS[:r19].inbound.stop_at('De Haro St & 23rd St'),
-	RS[:r48].inbound.stop_at('25th St & Wisconsin St'),
-	RS[:r48].outbound.stop_at('25th St & Wisconsin St'),
+  RS[:r10].inbound.stop_at('Wisconsin St & Madera St'),
+# RS[:r19].inbound.stop_at('De Haro St & 23rd St'),
+  RS[:r48].inbound.stop_at('25th St & Wisconsin St'),
+# RS[:r48].outbound.stop_at('25th St & Wisconsin St'),
 ]
 
 get '/' do
@@ -23,7 +23,7 @@ get '/' do
 		STOPS.map do |stop|
 			direction = stop.direction !~ /___O_/ ? 'Inbound' : 'Outbound'
 			key = "#{stop.route_tag} #{direction} at #{stop.title}"
-			routes[key] = stop.predictions
+			routes[key] = stop.predictions.take(2)
 		end
 
 		haml :index, locals: { routes: routes }
